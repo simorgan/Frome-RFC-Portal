9ea51107eaef8190835f4e2aca24e2ab14973d78
Simorgan <simonmorgan1000@hotmail.com>
2020-10-15 23:59:59 +0100

    update test

--- resources/views/pages/collectionSubmited.blade.php
+++ resources/views/pages/collectionSubmited.blade.php
@@ -1,1 +1,1 @@
-<h1>Collection Submited!!!! yeah it is working ID is: {{$idNumber}} this is a change on git hub</h1>
+<h1>Collection Submited!!!! yeah it is working ID is: {{$idNumber}} this is a change on git hub. i have seen this localy</h1>
